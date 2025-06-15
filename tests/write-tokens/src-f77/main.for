ca5b3697dea8ec57f31dce54d354817d1fd825e8
Lena Ickhorn <leickh@proton.me>
2025-06-15 18:10:41 +0200

    addition(tests): Add manual test: write-tokens
    The test 'write-tokens' tokenizes a file and writes out the tokens, as
    the name implies.

---
+++ tests/write-tokens/src-f77/main.for
@@ -0,0 +1,33 @@
+
+      PROGRAM json_write_tokens
+        USE json_parser
+        IMPLICIT NONE
+
+        INTEGER(4),POINTER :: tokens(:)
+        CHARACTER,POINTER :: source(:)
+
+        INTEGER(4) :: num_tokens
+        INTEGER :: file_status
+        INTEGER :: len_file
+        INTEGER :: file_id
+        INQUIRE(FILE="input.json", SIZE=len_file)
+        
+        OPEN(NEWUNIT=file_id, FILE="input.json", ACTION="read", IOSTAT= &
+     &file_status, FORM="UNFORMATTED", ACCESS="DIRECT", RECL=len_file)
+
+        IF(file_status .NE. 0) THEN
+          WRITE (*, *) "error: failed opening the json-file."
+          STOP
+        ENDIF
+
+        ALLOCATE(source(len_file))
+        READ(UNIT=file_id, IOSTAT=file_status, REC=1) source
+        CLOSE(UNIT=file_id)
+
+        CALL tokenize_json(source, len_file, tokens, num_tokens)
+        CALL write_tokens(tokens, num_tokens, source)
+
+        DEALLOCATE(tokens)
+        DEALLOCATE(source)
+      END
+
